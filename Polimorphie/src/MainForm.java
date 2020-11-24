5357a1bb317b014723e488591ca96f080d3fae90
bk201 <darkshadow20d>
2020-11-24 08:01:24 +0100

    Fixed the bmi status formula.

--- Polimorphie/src/MainForm.java
+++ Polimorphie/src/MainForm.java
@@ -87,7 +87,7 @@ public class MainForm {
                     bmi = weight / ((height / 100) * (height / 100));
                     bmiNumber.setText(String.valueOf(df2.format(bmi)));
 
-                    bmiStatus = bmiMenTitel(age,bmi,bmiStatus);
+                    bmiStatus = bmiTitel(age,bmi,bmiStatus);
 
                     bmiTitle.setText(bmiStatus);
                 }
@@ -95,142 +95,19 @@ public class MainForm {
         });
     }
 
-    public String bmiMenTitel(int age, double bmiN, String bmiStatus) {
+    public String bmiTitel(int age, double bmiN, String bmiStatus) {
 
-        if (age >= 16) {
             if (bmiN <= 18) {
                 return bmiStatus = "Untergewicht";
-            }
-            if (bmiN >= 20 && bmiN <= 24) {
-               return bmiStatus = "Normalgewicht";
-            }
-            if (bmiN >= 25 && bmiN <= 28) {
+            }else if (bmiN <= 25 ) {
+                return bmiStatus = "Normalgewicht";
+            }else if(bmiN <= 30) {
                 return bmiStatus = "leichtes Übergewicht";
-            }
-            if (bmiN >= 29) {
+            }else {
                 return bmiStatus = "Übergewicht";
             }
         }
-        if (age >= 17 && age <= 24) {
-            if (bmiN <= 18) {
-                return bmiStatus = "Untergewicht";
-            }
-            if (bmiN == 19 && bmiN <= 24) {
-                return bmiStatus = "Normalgewicht";
-            }
-            if (bmiN == 25 && bmiN <= 28) {
-                return bmiStatus = "leichtes Übergewicht";
-            }
-            if (bmiN >= 29) {
-               return bmiStatus = "Übergewicht";
-            }
 
-            if (age >= 25 && age <= 34) {
-                if (bmiN <= 19) {
-                 return bmiStatus = "Untergewicht";
-                }
-                if (bmiN == 20 && bmiN <= 25) {
-                    return bmiStatus = "Normalgewicht";
-                }
-                if (bmiN == 26 && bmiN <= 29) {
-                    return bmiStatus = "leichtes Übergewicht";
-                }
-                if (bmiN >= 30) {
-                    return bmiStatus = "Übergewicht";
-                }
-            }
-
-            if (age >= 35 && age <= 44) {
-                if (bmiN <= 21) {
-                    return bmiStatus = "Untergewicht";
-                }
-                if (bmiN == 22 && bmiN <= 27) {
-                    return bmiStatus = "Normalgewicht";
-                }
-                if (bmiN == 28 && bmiN <= 31) {
-                    return bmiStatus = "leichtes Übergewicht";
-                }
-                if (bmiN >= 32) {
-                    return bmiStatus = "Übergewicht";
-                }
-            }
-            if (age >= 45 && age <= 54) {
-                if (bmiN <= 22) {
-                    return bmiStatus = "Untergewicht";
-                }
-                if (bmiN == 23 && bmiN <= 28) {
-                    return bmiStatus = "Normalgewicht";
-                }
-                if (bmiN == 29 && bmiN <= 32) {
-                    return bmiStatus = "leichtes Übergewicht";
-                }
-                if (bmiN >= 33) {
-                    return bmiStatus = "Übergewicht";
-                }
-            }
-            if (age >= 55 && age <= 64) {
-                if (bmiN <= 23) {
-                    return bmiStatus = "Untergewicht";
-                }
-                if (bmiN == 24 && bmiN <= 29) {
-                    return bmiStatus = "Normalgewicht";
-                }
-                if (bmiN == 30 && bmiN <= 33) {
-                    return bmiStatus = "leichtes Übergewicht";
-                }
-                if (bmiN >= 34) {
-                    return bmiStatus = "Übergewicht";
-                }
-            }
-            if (age >= 65 && age <= 90) {
-                if (bmiN <= 24) {
-                    return bmiStatus = "Untergewicht";
-                }
-                if (bmiN == 25 && bmiN <= 30) {
-                    return bmiStatus = "Normalgewicht";
-                }
-                if (bmiN == 31 && bmiN <= 34) {
-                    return bmiStatus = "leichtes Übergewicht";
-                }
-                if (bmiN >= 35) {
-                    return bmiStatus = "Übergewicht";
-                }
-            }
-        }
-        return bmiStatus;
-    }
-
-    public void bmiWomanTitel(int age, double bmiN, String bmiStatus) {
-        if (age >= 16 && age <= 24) {
-            if (bmiN <= 18) {
-                bmiStatus = "Untergewicht";
-            }
-            if (bmiN == 19 && bmiN <= 24) {
-                bmiStatus = "Normalgewicht";
-            }
-            if (bmiN == 25 && bmiN <= 28) {
-                bmiStatus = "leichtes Übergewicht";
-            }
-            if (bmiN >= 29) {
-                bmiStatus = "Übergewicht";
-            }
-
-            if (age >= 25 && age <= 34) {
-                if (bmiN <= 19) {
-                    bmiStatus = "Untergewicht";
-                }
-                if (bmiN == 20 && bmiN <= 25) {
-                    bmiStatus = "Normalgewicht";
-                }
-                if (bmiN == 26 && bmiN <= 29) {
-                    bmiStatus = "leichtes Übergewicht";
-                }
-                if (bmiN >= 30) {
-                    bmiStatus = "Übergewicht";
-                }
-            }
-        }
-    }
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
